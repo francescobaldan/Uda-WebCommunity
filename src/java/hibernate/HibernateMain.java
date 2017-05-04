7a9ad5d7c1577d0711cff5d2b82736cdd4156f00
FSEVERI\sbrizza3331 <FSEVERI\sbrizza3331@linf4-06.fseveri.it>
2017-05-04 13:11:53 +0200

    mmkoòu890

--- src/java/hibernate/HibernateMain.java
+++ src/java/hibernate/HibernateMain.java
@@ -18,25 +18,7 @@ import java.util.List;
  */
 public class HibernateMain {
     public static void main(String[]args){
-        DAOEventi daoE=new DAOEventi();
-        Calendar d = Calendar.getInstance();
-        d.set(03, 04, 2017);
-        Integer EventoId1=daoE.addEvento(1, "Evento 1", "Padova", d);
-        d.set(10, 05, 2017);
-        EventoId1=daoE.addEvento(2, "Evento 2", "Padova", d);
-        d.set(28, 07, 2017);
-        EventoId1=daoE.addEvento(3, "Evento 3", "Padova", d);
-        List listaE=null;
-        listaE=daoE.showEventi();
-        
-        for(Iterator iterator=listaE.iterator();
-            iterator.hasNext();){
-            
-            Evento p=(Evento)iterator.next();
-            System.out.print("Id: "+p.getIdEvento());
-            System.out.print("  Nome: "+p.getTitolo());
-            System.out.print("  Luogo: "+p.getLuogo());
-            System.out.print("  Data: "+p.getData());
-        }
+
+       
     }
 }
